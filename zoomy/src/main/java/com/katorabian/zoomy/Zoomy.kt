ad3a2b92d95c44fd72b37efc25bff95fcfa13dbd
NarutoBratishka <katorabian@gmail.com>
2023-03-05 20:45:30 +0700

    refactor: convert Zoomy from Java to Kotlin

---
+++ zoomy/src/main/java/com/katorabian/zoomy/Zoomy.kt
@@ -0,0 +1,115 @@
+package com.katorabian.zoomy
+
+import android.app.Activity
+import android.app.Dialog
+import android.app.DialogFragment
+import android.view.View
+import android.view.animation.Interpolator
+
+/**
+ * Created by Álvaro Blanco Cabrero on 12/02/2017.
+ * Zoomy.
+ */
+object Zoomy {
+    private var mDefaultConfig = ZoomyConfig()
+    @JvmStatic
+    fun setDefaultConfig(config: ZoomyConfig) {
+        mDefaultConfig = config
+    }
+
+    fun unregister(view: View) {
+        view.setOnTouchListener(null)
+    }
+
+    class Builder {
+        private var mDisposed = false
+        private var mConfig: ZoomyConfig? = null
+        private var mTargetContainer: TargetContainer?
+        private var mTargetView: View? = null
+        private var mZoomListener: ZoomListener? = null
+        private var mZoomInterpolator: Interpolator? = null
+        private var mTapListener: TapListener? = null
+        private var mLongPressListener: LongPressListener? = null
+        private var mdDoubleTapListener: DoubleTapListener? = null
+
+        constructor(activity: Activity?) {
+            mTargetContainer = ActivityContainer(activity)
+        }
+
+        constructor(dialog: Dialog?) {
+            mTargetContainer = DialogContainer(dialog)
+        }
+
+        constructor(dialogFragment: DialogFragment?) {
+            mTargetContainer = DialogFragmentContainer(dialogFragment)
+        }
+
+        fun target(target: View?): Builder {
+            mTargetView = target
+            return this
+        }
+
+        fun animateZooming(animate: Boolean): Builder {
+            checkNotDisposed()
+            if (mConfig == null) mConfig = ZoomyConfig()
+            mConfig!!.isZoomAnimationEnabled = animate
+            return this
+        }
+
+        fun enableImmersiveMode(enable: Boolean): Builder {
+            checkNotDisposed()
+            if (mConfig == null) mConfig = ZoomyConfig()
+            mConfig!!.isImmersiveModeEnabled = enable
+            return this
+        }
+
+        fun interpolator(interpolator: Interpolator?): Builder {
+            checkNotDisposed()
+            mZoomInterpolator = interpolator
+            return this
+        }
+
+        fun zoomListener(listener: ZoomListener?): Builder {
+            checkNotDisposed()
+            mZoomListener = listener
+            return this
+        }
+
+        fun tapListener(listener: TapListener?): Builder {
+            checkNotDisposed()
+            mTapListener = listener
+            return this
+        }
+
+        fun longPressListener(listener: LongPressListener?): Builder {
+            checkNotDisposed()
+            mLongPressListener = listener
+            return this
+        }
+
+        fun doubleTapListener(listener: DoubleTapListener?): Builder {
+            checkNotDisposed()
+            mdDoubleTapListener = listener
+            return this
+        }
+
+        fun register() {
+            checkNotDisposed()
+            if (mConfig == null) mConfig = mDefaultConfig
+            requireNotNull(mTargetContainer) { "Target container must not be null" }
+            requireNotNull(mTargetView) { "Target view must not be null" }
+            mTargetView!!.setOnTouchListener(
+                ZoomableTouchListener(
+                    mTargetContainer!!, mTargetView!!, mConfig!!,
+                    mZoomInterpolator, mZoomListener, mTapListener,
+                    mLongPressListener, mdDoubleTapListener
+                )
+            )
+            mDisposed = true
+        }
+
+        private fun checkNotDisposed() {
+            check(!mDisposed) { "Builder already disposed" }
+        }
+    }
+}
